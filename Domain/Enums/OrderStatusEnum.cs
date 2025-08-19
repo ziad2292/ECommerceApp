4f2a481e4e94c240f643c9802eb34b258923d308
Ziad Ahmad <Ziad.Ahmad.intern@linkdevelopment.com>
2025-08-19 17:10:07 +0300

    Add Enums

---
+++ Domain/Enums/OrderStatusEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum OrderStatusEnum
+    {
+        Pending = 1,
+        Confirmed = 2,
+        Cancelled = 3
+    }
+}
