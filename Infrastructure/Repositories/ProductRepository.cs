4f7bbce3d9a0ffa0b12f126bfb4ce9206353b10b
Ziad Ahmad <Ziad.Ahmad.intern@linkdevelopment.com>
2025-09-23 12:17:58 +0300

    Implemented product repo interface and class

---
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using Application.Intefraces.Repositories;
+using Domain.Entities;
+using Infrastructure.Persistence._Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Product>> GetProductsByCategoryAsync(int categoryId)
+        {
+            return await _context.Products.Where(p => p.CategoryId == categoryId).ToListAsync();
+        }
+
+        public Task<List<Product>> GetProductsByName(string name)
+        {
+            return _context.Products
+                .Where(p => p.Name!.Contains(name))
+                .ToListAsync();
+        }
+
+        public Task<List<Product>> GetProductsByPriceRangeAsync(decimal minPrice, decimal maxPrice)
+        {
+            return _context.Products
+                .Where(p => p.Price >= minPrice && p.Price <= maxPrice)
+                .ToListAsync();
+        }
+    }
+}
